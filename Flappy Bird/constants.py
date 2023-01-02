b8070a814932d624ba2630e902620e0b0509f283
Salim Ali <75537889+Salim-Ali-94@users.noreply.github.com>
2023-01-02 19:17:43 +0200

    Update constants.py

--- Flappy Bird/constants.py
+++ Flappy Bird/constants.py
@@ -10,6 +10,7 @@ size = [screen_width, screen_height]
 white = (255, 255, 255)
 scroll_speed = 4
 ground_scroll = 0
+ground_height = 768
 time_previous = pg.time.get_ticks() - random.randint(1500, 100000)
 framerate = 60
 screen = pg.display.set_mode(size)
